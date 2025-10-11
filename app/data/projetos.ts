36a175d8f5519b496c87660a2c6c52474b501aba
RafaelAquinodev <oaquinorafael@gmail.com>
2025-10-11 19:56:19 -0300

    fix: removendo projetos

--- app/data/projetos.ts
+++ app/data/projetos.ts
@@ -20,24 +20,24 @@ export const projetosData: Projeto[] = [
       "https://github.com/RafaelAquinodev/questionario-radar-competencia-ia",
     siteUrl: "#",
   },
-  {
-    id: 3,
-    title: "Quiz Perfil Profissional IA",
-    description:
-      "Questionário interativo de autodiagnóstico sobre o uso de Inteligência Artificial. O projeto propõe uma reflexão sobre o perfil profissional em relação à IA, gerando um relatório final com diagnóstico e recomendações para evolução na jornada com a tecnologia",
-    imageUrl: "/perfil-profissional.jpg",
-    githubUrl: "https://github.com/RafaelAquinodev/quiz-ai",
-    siteUrl: "https://quiz-perfil-profissional-ia.vercel.app/",
-  },
-  {
-    id: 4,
-    title: "Quiz Maturidade IA",
-    description:
-      "Questionário de avaliação da maturidade organizacional para adoção de Inteligência Artificial. A ferramenta analisa dados, tecnologia, processos, cultura e estratégia, entregando um panorama claro do estágio atual da empresa e recomendações para a jornada de transformação com IA.",
-    imageUrl: "/maturidade-ia.jpg",
-    githubUrl: "https://github.com/RafaelAquinodev/maturidade-ai",
-    siteUrl: "https://maturidadequiz-ia.vercel.app/", // Use '#' se não houver site ao vivo
-  },
+  // {
+  //   id: 3,
+  //   title: "Quiz Perfil Profissional IA",
+  //   description:
+  //     "Questionário interativo de autodiagnóstico sobre o uso de Inteligência Artificial. O projeto propõe uma reflexão sobre o perfil profissional em relação à IA, gerando um relatório final com diagnóstico e recomendações para evolução na jornada com a tecnologia",
+  //   imageUrl: "/perfil-profissional.jpg",
+  //   githubUrl: "https://github.com/RafaelAquinodev/quiz-ai",
+  //   siteUrl: "https://quiz-perfil-profissional-ia.vercel.app/",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Quiz Maturidade IA",
+  //   description:
+  //     "Questionário de avaliação da maturidade organizacional para adoção de Inteligência Artificial. A ferramenta analisa dados, tecnologia, processos, cultura e estratégia, entregando um panorama claro do estágio atual da empresa e recomendações para a jornada de transformação com IA.",
+  //   imageUrl: "/maturidade-ia.jpg",
+  //   githubUrl: "https://github.com/RafaelAquinodev/maturidade-ai",
+  //   siteUrl: "https://maturidadequiz-ia.vercel.app/", // Use '#' se não houver site ao vivo
+  // },
   {
     id: 5,
     title: "VisionXdoc",
